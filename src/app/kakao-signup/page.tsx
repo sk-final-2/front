cee571d869a3703c98edb022a24f5aa791ebacff
minju3212 <smj3mjs@gmail.com>
2025-07-30 15:52:59 +0900

    feat: 로그인,소셜로그인,회원가입

---
+++ src/app/kakao-signup/page.tsx
@@ -0,0 +1,9 @@
+import KakaoForm from '@/components/kakao/KakaoForm';
+
+export default function KakaoPage() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <KakaoForm />
+    </div>
+  );
+}
